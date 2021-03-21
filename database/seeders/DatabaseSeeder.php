5cc91636f7f2db9ae2bfc999fe791009a847c482
John George <Sci.JohnGeorge.2017@alexu.edu.eg>
2021-03-21 20:01:50 +0200

    Fix Seeder Database Seeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        Post::factory(500)->create();
+        $this->call(PostSeeder::class);
     }
 }
